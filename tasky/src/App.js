29449330de0b0e926e244004200e0aa0ca736f32
Yanbo-Yun <20109110@mail.wit.ie>
2024-09-27 21:35:47 +0800

    Completed Tasky Lab 1

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" >
-        Empty diswasher
+      <Task title="Dishes" deadline="Today" description="Empty diswasher">
+        
       </Task>   
-      <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away">
+      
       </Task> 
       <Task title="Tidy" deadline="Today" />
        
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -6,7 +6,7 @@ const Task = (props) => {
         <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
-            <p><i>{props.children}</i></p>
+            <p><i>{props.description}</i></p>
         </div>
     )
 }
